bd741f80bf86b5d9d8db42ebe6ed17e461371a9f
David Vargas Hernandez <david-0610@live.com.mx>
2022-09-30 13:39:16 -0500

    add new page for character details

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,19 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useConsumeContext } from "../hooks/useConsumeContext";
 
 export const Characters = ({ character, addToFavorite }) => {
+  const { deleteFavorites, favoritesChecker } = useConsumeContext();
+
+  const url = character.url;
+  const getId = url.substring(29, 30);
+
   return (
     <div className="col-md-4">
       <div className="text-center card card-body p-5">
-        <h3>{character.name}</h3>
-        <p>Height: {character.height} cm.</p>
-        <p>Mass: {character.mass} KG</p>
-        <p>Birth year: {character.birth_year}</p>
-        <button
-          className="btn btn-success"
-          onClick={() => addToFavorite(character)}
-        >
-          Add to favorites
-        </button>
+        <Link to={`/character/${getId}`}>
+          <h3>{character.name}</h3>
+          <p>Height: {character.height} cm.</p>
+          <p>Mass: {character.mass} KG</p>
+          <p>Birth year: {character.birth_year}</p>
+        </Link>
+        {favoritesChecker(character.url) ? (
+          <button
+            className={"btn btn-danger"}
+            onClick={() => deleteFavorites(character)}
+          >
+            Remove to favorites
+          </button>
+        ) : (
+          <button
+            className={"btn btn-success"}
+            onClick={() => addToFavorite(character)}
+          >
+            Add to favorites
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useConsumeContext } from "../hooks/useConsumeContext";
 
 export const Character = () => {
-  return <div>Character</div>;
+  const {
+    getCharacter,
+    onlyCharacter,
+    addToFavorite,
+    deleteFavorites,
+    favoritesChecker,
+  } = useConsumeContext();
+  let { id } = useParams();
+
+  useEffect(() => {
+    getCharacter(id);
+  }, []);
+
+  return (
+    <div className="col-md-12">
+      <div className="text-center card card-body p-5">
+        <h3>{onlyCharacter.name}</h3>
+        <p>Height: {onlyCharacter.height} cm.</p>
+        <p>Mass: {onlyCharacter.mass} KG.</p>
+        <p>Hair color: {onlyCharacter.hair_color}</p>
+        <p>Skin color: {onlyCharacter.skin_color}</p>
+        <p>Eye color: {onlyCharacter.eye_color}</p>
+        <p>Birth year: {onlyCharacter.birth_year}</p>
+        <p>Gender: {onlyCharacter.gender}</p>
+        {favoritesChecker(onlyCharacter.url) ? (
+          <button
+            className={"btn btn-danger"}
+            onClick={() => deleteFavorites(onlyCharacter)}
+          >
+            Remove to favorites
+          </button>
+        ) : (
+          <button
+            className={"btn btn-success"}
+            onClick={() => addToFavorite(onlyCharacter)}
+          >
+            Add to favorites
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
